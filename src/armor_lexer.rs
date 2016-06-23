5ba0b604f69c8c1fa20043b65c4193c76fc9fd91
LambdaXymox <lambda.xymox@gmail.com>
2016-06-22 23:56:01 -0500

    Implement tokenizer for AsciiArmor.

---
+++ src/armor_lexer.rs
@@ -0,0 +1,69 @@
+use std::str::Chars;
+use std::iter::Iterator;
+
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum TokenType {
+    Character,
+    NewLine,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct ArmorToken {
+    token_type: TokenType,
+    token: char,
+}
+
+impl ArmorToken {
+    pub fn new(token_type: TokenType, token: char) -> ArmorToken {
+        ArmorToken {
+            token_type: token_type,
+            token: token,
+        }
+    }
+
+    pub fn valid_token(&self) -> bool {
+        match self.token {
+            '\n' | '\r' => self.token_type == TokenType::NewLine,
+            _           => self.token_type == TokenType::Character, 
+        }
+    }
+}
+
+pub struct ArmorLexer<'a> {
+    stream: String,
+    chars:  Chars<'a>,
+}
+
+impl<'a> ArmorLexer<'a> {
+    pub fn new(stream: &String) -> ArmorLexer {
+        let new_stream = stream.clone();
+
+        ArmorLexer {
+            stream: new_stream,
+            chars:  stream.chars(),
+        }
+    }
+
+    pub fn next_token(&mut self) -> Option<ArmorToken> {
+        let next_char = self.chars.next();
+
+        match next_char {
+            None                    => None,
+            Some('\n') | Some('\r') => Some(ArmorToken::new(TokenType::NewLine, '\n')),
+            Some(character)         => Some(ArmorToken::new(TokenType::Character, character)), 
+        } 
+    }
+}
+
+impl<'a> Iterator for ArmorLexer<'a> {
+    type Item = ArmorToken;
+
+    fn next(&mut self) -> Option<ArmorToken> {
+        self.next_token()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.chars.size_hint()
+    }
+}
