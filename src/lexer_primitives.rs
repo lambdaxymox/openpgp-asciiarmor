fec3706a0fda4f6dc79c417954b812f3d7ce7e96
Stallmanifold <stallmanifold@gmail.com>
2016-07-26 19:52:12 -0500

    Remove unused PhantomData.

--- src/lexer_primitives.rs
+++ src/lexer_primitives.rs
@@ -1,4 +1,3 @@
-use std::marker::PhantomData;
 use combine::combinator::Token;
 use combine::primitives::Stream;
 use combine::{Parser, ParseResult, ParseError};
@@ -6,10 +5,26 @@ use combine::char;
 use combine::char::{Upper, Lower, Space, CrLf};
 
 
+macro_rules! lexer_combinator_impl {
+    ($name:ty, $stream_type:ty, $inner_parser_type:ty) => {
+        impl<$stream_type> Parser for $name<$stream_type> where $stream_type: Stream<Item=char> {
+            type Input = I;
+            type Output = <$inner_parser_type as Parser>::Output;
+
+            fn parse_lazy(&mut self, input: Self::Input) -> ParseResult<Self::Output, Self::Input> {
+                self.inner.parse_lazy(input)
+            }
+
+            fn add_error(&mut self, _error: &mut ParseError<Self::Input>) {
+                self.inner.add_error(_error);
+            }
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct UpperCaseLetter<I> where I: Stream<Item=char> {
     inner: Upper<I>,
-    _marker: PhantomData<I>,
 }
 
 pub fn uppercase_letter<I>() -> UpperCaseLetter<I> 
@@ -17,7 +32,6 @@ pub fn uppercase_letter<I>() -> UpperCaseLetter<I>
 
     UpperCaseLetter {
         inner: char::upper(),
-        _marker: PhantomData,
     }
 }
 
@@ -37,7 +51,6 @@ impl<I> Parser for UpperCaseLetter<I> where I: Stream<Item=char> {
 #[derive(Clone)]
 pub struct LowerCaseLetter<I> where I: Stream<Item=char> {
     inner: Lower<I>,
-    _marker: PhantomData<I>,
 }
 
 pub fn lowercase_letter<I>() -> LowerCaseLetter<I> 
@@ -45,7 +58,6 @@ pub fn lowercase_letter<I>() -> LowerCaseLetter<I>
 
     LowerCaseLetter {
         inner: char::lower(),
-        _marker: PhantomData,
     }
 
 }
@@ -66,13 +78,11 @@ impl<I> Parser for LowerCaseLetter<I> where I: Stream<Item=char> {
 #[derive(Clone)]
 pub struct EqualSign<I> where I: Stream<Item=char> {
     inner: Token<I>,
-    _marker: PhantomData<I>,
 }
 
 pub fn equal_sign<I>() -> EqualSign<I> where I: Stream<Item=char> {
     EqualSign { 
         inner: char::char('='),
-        _marker: PhantomData,
     }
 }
 
@@ -92,13 +102,11 @@ impl<I> Parser for EqualSign<I> where I: Stream<Item=char> {
 #[derive(Clone)]
 pub struct Colon<I> where I: Stream<Item=char> {
     inner: Token<I>,
-    _marker: PhantomData<I>,
 }
 
 pub fn colon<I>() -> Colon<I> where I: Stream<Item=char> {
     Colon {
         inner: char::char(':'),
-        _marker: PhantomData,
     }
 }
 
@@ -118,13 +126,11 @@ impl<I> Parser for Colon<I> where I: Stream<Item=char> {
 #[derive(Clone)]
 pub struct Digit<I> where I: Stream<Item=char> {
     inner: char::Digit<I>,
-    _marker: PhantomData<I>,
 }
 
 pub fn digit<I>() -> Digit<I> where I: Stream<Item=char> {
     Digit {
         inner: char::digit(),
-        _marker: PhantomData,
     }
 }
 
@@ -144,13 +150,11 @@ impl<I> Parser for Digit<I> where I: Stream<Item=char> {
 #[derive(Clone)]
 pub struct WhiteSpace<I> where I: Stream<Item=char> {
     inner: Space<I>,
-    _marker: PhantomData<I>,
 }
 
 pub fn whitespace<I>() -> WhiteSpace<I> where I: Stream<Item=char> {
     WhiteSpace {
         inner: char::space(),
-        _marker: PhantomData,
     }
 }
 
@@ -170,13 +174,11 @@ impl<I> Parser for WhiteSpace<I> where I: Stream<Item=char> {
 #[derive(Clone)]
 pub struct Comma<I> where I: Stream<Item=char> {
     inner: Token<I>,
-    _marker: PhantomData<I>,
 }
 
 pub fn comma<I>() -> Comma<I> where I: Stream<Item=char> {
     Comma {
         inner: char::char(','),
-        _marker: PhantomData,
     }
 }
 
@@ -196,13 +198,11 @@ impl<I> Parser for Comma<I> where I: Stream<Item=char> {
 #[derive(Clone)]
 pub struct ForwardSlash<I> where I: Stream<Item=char> {
     inner: Token<I>,
-    _marker: PhantomData<I>,
 }
 
 pub fn forward_slash<I>() -> ForwardSlash<I> where I: Stream<Item=char> {
     ForwardSlash {
         inner: char::char('/'),
-        _marker: PhantomData,
     }
 }
 
@@ -222,13 +222,11 @@ impl<I> Parser for ForwardSlash<I> where I: Stream<Item=char> {
 #[derive(Clone)]
 pub struct NewLine<I> where I: Stream<Item=char> {
     inner: char::NewLine<I>,
-    _marker: PhantomData<I>,
 }
 
 pub fn newline<I>() -> NewLine<I> where I: Stream<Item=char> {
     NewLine {
         inner: char::newline(),
-        _marker: PhantomData,
     }
 }
 
@@ -245,6 +243,19 @@ impl<I> Parser for NewLine<I> where I: Stream<Item=char> {
     }
 }
 
+/*
+pub struct ArmorLexer<I> where I: Stream<Item=char> {
+    inner: 
+}
+
+pub fn armor_lexer<I>() -> ArmorLexer<I>
+    where I: Stream<item=char> {
+
+    ArmorLexer {
+        inner: 
+    }
+}
+*/
 
 #[cfg(test)]
 mod tests {
